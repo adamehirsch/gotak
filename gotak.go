c1382b926213d7ddd30ce0d131141276bdea82dc
Adam Hirsch <adam.e.hirsch@gmail.com>
2017-03-01 10:15:55 -0600

    added UUIDs to boards

--- gotak.go
+++ gotak.go
@@ -2,57 +2,82 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	// "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
 )
 
 // Piece is the most basic element of a Tak game. One of two colors; one of three types.
 type Piece struct {
 	// one of "black" or "white"
-	Color string
+	color string
 	// Type can be one of "flat", "standing", or "capstone"
-	Type string
+	orientation string
 }
 
-// Stack is probably a needless piece of structure; it's just a slice of Pieces
+// Stack is potentially a needless piece of structure; it's just a slice of Pieces
 type Stack struct {
 	// the top of the stack is at [0]
-	Pieces []Piece
+	pieces []Piece
 }
 
-func SlashHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Gorilla!\n"))
+// Board is an NxN grid of spaces, optionally occupied by Stacks of Pieces. A given Board has a guaranteed unique uuid
+type Board struct {
+	uuid uuid.UUID
+	grid [][]Stack
 }
+
+// func MakeRandomBoard() Board {
+// 	firstBoard := MakeGameBoard(3)
+//
+// 	firstPiece := Piece{"white", "flat"}
+// 	secondPiece := Piece{"black", "flat"}
+// 	firstBoard.grid[0][0] = Stack{[]Piece{firstPiece, secondPiece}}
+// 	return firstBoard
+// }
 
 // MakeGameBoard takes an integer size and returns a slice of slices of Stacks
-func MakeGameBoard(size int) [][]Stack {
+func MakeGameBoard(size int) Board {
+	// each board gets a unique, random UUIDv4
+	newUUID := uuid.NewV4()
 
-	board := make([][]Stack, size, size)
+	// first make the rows...
+	newGrid := make([][]Stack, size, size)
 
+	// ... then populate with the columns of spaces
 	for i := 0; i < size; i++ {
 		row := make([]Stack, size, size)
-		board[i] = row
+		newGrid[i] = row
 	}
-	return board
+
+	newBoard := Board{newUUID, newGrid}
+	// newBoard.grid = newGrid
+	// fmt.Println("2", newBoard)
+	// newBoard.uuid = newUUID
+	// fmt.Println("3", newBoard)
+
+	return newBoard
+}
+
+func NewGameHandler(w http.ResponseWriter, r *http.Request) {
+	return
 }
 
 func main() {
 
-	firstBoard := MakeGameBoard(3)
-	firstPiece := Piece{"white", "flat"}
-	secondPiece := Piece{"black", "flat"}
+	testBoard := MakeGameBoard(2)
+	fmt.Println(testBoard)
 
-	firstBoard[0][0] = Stack{[]Piece{firstPiece, secondPiece}}
-	fmt.Println(firstBoard)
+	// r := mux.NewRouter()
+	// // Routes consist of a path and a handler function.
+	// r.HandleFunc("/", SlashHandler)
+	// r.HandleFunc("/newgame", NewGameHandler)
+	//
+	// // Bind to a port and pass our router in
+	// log.Fatal(http.ListenAndServe(":8000", r))
+}
 
-	r := mux.NewRouter()
-	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", SlashHandler)
-	r.HandleFunc("/newgam", SlashHandler)
-
-	// Bind to a port and pass our router in
-	log.Fatal(http.ListenAndServe(":8000", r))
+func SlashHandler(w http.ResponseWriter, r *http.Request) {
+	// fooBoard := MakeRandomBoard()
+	w.Write([]byte("Gorilla!\n"))
 }
